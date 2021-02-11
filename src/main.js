36a4e6c74b8885bfd82fe9775bd6a54281248083
Natalia-BP <nataliabarqueroperez@gmail.com>
2021-02-11 02:25:15 +0000

    ejercicio excuse generator terminado

--- src/main.js
+++ src/main.js
@@ -6,8 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = () => {
-  document.querySelector("h2.excuse").innerHTML = myExcuse();
+  document.querySelector("button.excuse").addEventListener("click", () => {
+    document.querySelector("h1.excuse").innerHTML = myExcTitle();
+    document.querySelector("h2.excuse").innerHTML = myExcuse();
+  });
 };
+
+function myExcTitle() {
+  let title = [
+    "OMG! This will BLOW your mind...",
+    "I know this sounds too weird, but it reeeaaally happened...",
+    "GEEZUS LORD! This just happened to me...",
+    "HOLY COW! You are NOT gonna believe this..",
+    "I swear this happened!"
+  ];
+
+  let rndTitle = Math.floor(Math.random() * title.length);
+
+  let finalTitle = `${title[rndTitle]}`;
+
+  return finalTitle;
+}
 
 function myExcuse() {
   // Variables
@@ -24,22 +43,25 @@ function myExcuse() {
     "The rain",
     "The wind",
     "My parents",
-    "The love of my life",
-    "A raging storm"
+    "A raging storm",
+    "Voldemort"
   ];
+
   let action = [
     "ate my",
-    "peed on",
-    "destroyed the",
+    "peed on my",
+    "destroyed my",
     "swallowed my",
     "ripped to pieces my",
-    "crushed the",
-    "vanished the",
+    "crushed my",
+    "vanished my",
     "burned my",
     "created a golem that destroyed my",
     "desecrated my",
-    "bewitched my sister into ripping my"
+    "bewitched my sister into ripping my",
+    "broke into my house and stole my"
   ];
+
   let what = [
     "homework",
     "keys",
@@ -48,16 +70,30 @@ function myExcuse() {
     "walls",
     "thesis presentation",
     "will to live",
-    "final project"
+    "final project",
+    "christmas gifts",
+    "nice sweater",
+    "running shoes",
+    "TV",
+    "outfit",
+    "purse",
+    "glasses",
+    "clothes"
   ];
+
   let when = [
     "right before my class",
     "just when I was leaving",
     "when I got to school",
-    "when I finished",
     "during my lunch break",
     "at breakfast",
-    "the night before"
+    "the night before",
+    "while I was sleeping",
+    "while I was out",
+    "while I was taking a shower",
+    "yesterday",
+    "a few minutes ago",
+    "just an hour before"
   ];
 
   // Random numbers
